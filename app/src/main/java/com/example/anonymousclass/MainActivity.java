f9ded413ac50045be9f8754b625209ad63cf6b18
S-B choi <sb112233@naver.com>
2021-04-01 14:38:00 +0900

    Anoynmous inner class for 2 buttons

--- app/src/main/java/com/example/anonymousclass/MainActivity.java
+++ app/src/main/java/com/example/anonymousclass/MainActivity.java
@@ -7,24 +7,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
-    Button mButton1;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mButton1, mButton2;
     TextView mTextView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mButton1 = findViewById(R.id.button1);
+        mButton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-    mTextView1.setText("You clicked button 1");
-    }
+        switch (v.getId()) {
+            case R.id.button1:
+                mTextView1.setText("You clicked button 1");
+                break;
+            case R.id.button2:
+                mTextView1.setText("You clicked button 2");
+                break;
+            default:
+                break;
 
+        }
+    }
 }
